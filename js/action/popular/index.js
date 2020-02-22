8770b8925e23a03934ff0aad85a5a5aae0f79392
James <james@zhanzixiandeMacBook-Pro.local>
2020-02-22 22:31:03 +0800

    update

--- js/action/popular/index.js
+++ js/action/popular/index.js
@@ -1,32 +1,95 @@
 import Types from '../types';
 import DataStore from '../../expand/dao/DataStore';
+import types from '../types';
 
 /**
  * 获取最热数据的异步action
  * @param storeName
  * @param url
- * @returns {function(*=)}
+ * @param pageSize
+ * @returns {function(*)}
  */
-export function onLoadPopularData(storeName, url) {
+export function onRefreshPopular(storeName, url, pageSize) {
   return dispatch => {
     dispatch({type: Types.POPULAR_REFRESH, storeName});
     let dataStore = new DataStore();
     dataStore
       .fetchData(url) //异步action与数据流
       .then(data => {
-        handleData(dispatch, storeName, data);
+        handleData(dispatch, storeName, data, pageSize);
       })
       .catch(error => {
         console.log(error);
-        dispatch({type: Types.LOAD_POPULAR_FAIL, storeName, error});
+        dispatch({type: Types.POPULAR_REFRESH_FAIL, storeName, error});
       });
   };
 }
 
-function handleData(dispatch, storeName, data) {
+/**
+ * 加载更多
+ * @param storeName
+ * @param pageIndex 第几页
+ * @param pageSize 每页显示条数
+ * @param dataArray 原始数据
+ * @param callBack 回调函数，可以通过回调函数来向调用页面通信：比如异常信息的展示，没有更多的等待
+ * @returns {function(*)}
+ */
+export function onLoadMorePopular(
+  storeName,
+  pageIndex,
+  pageSize,
+  dataArray = [],
+  callBack,
+) {
+  return dispatch => {
+    setTimeout(() => {
+      // 模拟网络请求
+      if ((pageIndex - 1) * pageSize >= dataArray.length) {
+        //已加载全部数据
+        if (typeof callBack === 'function') {
+          callBack('no more');
+        }
+        dispatch({
+          type: types.POPULAR_LOAD_MORE_FAIL,
+          error: 'no more',
+          storeName: storeName,
+          pageIndex: --pageIndex,
+          projectModes: dataArray,
+        });
+      } else {
+        // 本次和载入的最大数量
+        let max =
+          pageSize * pageIndex > dataArray.length
+            ? dataArray.length
+            : pageSize * pageIndex;
+        dispatch({
+          type: types.POPULAR_LOAD_MORE_SUCCESS,
+          storeName,
+          pageIndex,
+          projectModes: dataArray.slice(0, max),
+        });
+      }
+    }, 500);
+  };
+}
+
+/**
+ * 处理下拉刷新的数据
+ * @param dispatch
+ * @param storeName
+ * @param data
+ * @param pageSize
+ */
+function handleData(dispatch, storeName, data, pageSize) {
+  let fixItems = [];
+  if (data && data.data && data.data.items) {
+    fixItems = data.data.items;
+  }
   dispatch({
-    type: Types.LOAD_POPULAR_SUCCESS,
-    items: data && data.data && data.data.items,
+    type: Types.POPULAR_REFRESH_SUCCESS,
+    projectModes:
+      pageSize > fixItems.length ? fixItems : fixItems.slice(0, pageSize),
     storeName,
+    pageIndex: 1,
   });
 }
--- js/page/PopularPage.js
+++ js/page/PopularPage.js
@@ -4,14 +4,16 @@ import {createMaterialTopTabNavigator} from 'react-navigation-tabs';
 import {
   StyleSheet,
   View,
-  Text,
+  ActivityIndicator,
   SafeAreaView,
   FlatList,
+  Text,
   RefreshControl,
 } from 'react-native';
 import {connect} from 'react-redux';
 import actions from '../action/index';
 import PopularItem from '../common/PopularItem';
+import Toast from 'react-native-easy-toast';
 
 const URL = 'https://api.github.com/search/repositories?q=';
 const QUERY_STR = '&sort=stars';
@@ -59,6 +61,7 @@ export default class PopularPage extends Component {
   }
 }
 
+const pageSize = 10; //设为常量，防止修改
 class PopularTab extends Component {
   constructor(props) {
     super(props);
@@ -70,14 +73,46 @@ class PopularTab extends Component {
     this.loadData();
   }
 
-  loadData() {
-    const {onLoadPopularData} = this.props;
+  loadData(loadMore) {
+    const {onRefreshPopular, onLoadMorePopular} = this.props;
+    const store = this._store();
     const url = this.genFetchUrl(this.storeName);
-    onLoadPopularData(this.storeName, url);
+    if (loadMore) {
+      onLoadMorePopular(
+        this.storeName,
+        ++store.pageIndex,
+        pageSize,
+        store.items,
+        callBack => {
+          this.refs.toast.show('没有更多了');
+        },
+      );
+    } else {
+      onRefreshPopular(this.storeName, url, pageSize);
+    }
   }
 
   genFetchUrl(key) {
     return URL + key + QUERY_STR;
+  }
+
+  /**
+   * 获取与当前页面有关的数据
+   * @returns {*}
+   * @private
+   */
+  _store() {
+    const {popular} = this.props;
+    let store = popular[this.storeName];
+    if (!store) {
+      store = {
+        items: [],
+        isLoading: false,
+        projectModes: [], //要显示的数据
+        hideLoadingMore: true, //默认隐藏加载更多
+      };
+    }
+    return store;
   }
 
   renderItem(data) {
@@ -85,19 +120,21 @@ class PopularTab extends Component {
     return <PopularItem item={item} onSelect={() => {}} />;
   }
 
+  genIndicator() {
+    return this._store().hideLoadingMore ? null : (
+      <View style={styles.indicatorContainer}>
+        <ActivityIndicator style={styles.indicator} />
+        <Text>正在加载更多</Text>
+      </View>
+    );
+  }
+
   render() {
-    const {popular} = this.props;
-    let store = popular[this.storeName];
-    if (!store) {
-      store = {
-        items: [],
-        isLoading: false,
-      };
-    }
+    let store = this._store();
     return (
       <View style={styles.container}>
         <FlatList
-          data={store.items}
+          data={store.projectModes}
           renderItem={data => this.renderItem(data)}
           keyExtractor={item => '' + item.id}
           refreshControl={
@@ -110,7 +147,13 @@ class PopularTab extends Component {
               tintColor={THEME_COLOR}
             />
           }
+          ListFooterComponent={() => this.genIndicator()}
+          onEndReached={() => {
+            this.loadData(true);
+          }}
+          onEndReachedThreshold={0.5}
         />
+        <Toast ref={'toast'} position={'center'} />
       </View>
     );
   }
@@ -121,8 +164,19 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  onLoadPopularData: (storeName, url) => {
-    dispatch(actions.onLoadPopularData(storeName, url));
+  onRefreshPopular: (storeName, url, pageSize) => {
+    dispatch(actions.onRefreshPopular(storeName, url, pageSize));
+  },
+  onLoadMorePopular: (storeName, pageIndex, pageSize, items, callBack) => {
+    dispatch(
+      actions.onLoadMorePopular(
+        storeName,
+        pageIndex,
+        pageSize,
+        items,
+        callBack,
+      ),
+    );
   },
 });
 
@@ -146,5 +200,12 @@ const styles = StyleSheet.create({
     fontSize: 13,
     marginTop: 6,
     marginBottom: 6,
+  },
+  indicatorContainer: {
+    alignItems: 'center',
+  },
+  indicator: {
+    color: 'red',
+    margin: 10,
   },
 });
