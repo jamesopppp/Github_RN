182a3d2151397ed8c2cade67e9c4062c120470c1
James <james@zhanzixiandeMacBook-Pro.local>
2020-05-05 23:20:55 +0800

    update

--- js/page/HomePage.js
+++ js/page/HomePage.js
@@ -4,14 +4,20 @@ import {NavigationAction} from 'react-navigation';
 import {connect} from 'react-redux';
 import NavigationUtil from '../navigator/NavigationUtil';
 import DynamicTabNavigator from '../navigator/DynamicTabNavigator';
+import BackPressComponent from '../common/BackPressComponent';
 
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.backPress = new BackPressComponent({backPress: this.onBackPress()});
+  }
+
   componentDidMount() {
-    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+    this.backPress.componentDidMount();
   }
 
   componentWillUnmount() {
-    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+    this.backPress.componentWillUnmount();
   }
 
   onBackPress = () => {
